3685050a6cc43b8bf94d5d6a7bfb1ebde62979e4
Gary Liu <gary.liu1119@gmail.com>
2019-01-14 08:15:57 -0600

    Create README.mk

---
+++ mule-maven-plugin/README.mk
@@ -0,0 +1,9 @@
+# mule-maven-plugin
+## **Overview**
+
+## **Details**
+Most details can be referred at [Mulesoft documents for Maven Plugin](https://docs.mulesoft.com/mule-runtime/4.1/ch-deploy-mule-application-mmp-task)
+
+## **Run The Maven Project**
+
+`mvn clean package deploy -DmuleDeploy -Dmaven.properties=src/main/resources/maven.properties`
