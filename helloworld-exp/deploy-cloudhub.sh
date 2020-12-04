a23468058c7f0c028fd11db154bdcc95ccc37cb3
Gary Liu <gary.liu1119@gmail.com>
2020-12-03 21:04:11 -0600

    add helloworld-exp

--- helloworld-exp/deploy-cloudhub.sh
+++ helloworld-exp/deploy-cloudhub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 TARGET=hammond
-mvn -e -X deploy -DskipTests -DmuleDeploy -Dmaven.properties=src/main/resources/deploy/${TARGET}.rtf.deploy.properties
+mvn -e -X deploy -DskipTests -DmuleDeploy -Dmaven.properties=src/main/resources/deploy/${TARGET}.ch.deploy.yaml
